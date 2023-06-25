2e9ee483efaa8b54f41f13aab3dfe38a5662e373
ceepu8 <caophuonguyen132002@gmail.com>
2023-06-26 00:38:07 +0700

    building layout, sidebar and button

---
+++ components/SidebarItem.tsx
@@ -0,0 +1,34 @@
+import Link from "next/link";
+import { IconType } from "react-icons";
+import { twMerge } from "tailwind-merge";
+
+interface SidebarItemProps {
+  icon: IconType;
+  label: string;
+  active?: boolean;
+  href: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+  icon: Icon,
+  label,
+  active,
+  href,
+}) => {
+  return (
+    <Link
+      href={href}
+      className={twMerge(
+        "flex flex-row h-auto items-center w-full gap-x-5 py-1",
+        "text-md font-medium text-neutral-400",
+        "cursor-pointer hover:text-white transition",
+        active && "text-white"
+      )}
+    >
+      <Icon size={26} />
+      <p className="truncate w-full">{label}</p>
+    </Link>
+  );
+};
+
+export default SidebarItem;
