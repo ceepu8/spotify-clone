1893cfb63ef70b3e89ee7da794cc7a149d8e3798
ceepu8 <caophuonguyen132002@gmail.com>
2023-06-27 19:15:50 +0700

    build upload modal UI

---
+++ components/UploadModal.tsx
@@ -0,0 +1,81 @@
+import useUploadModal from "@/hooks/useUploadModal";
+import Modal from "./Modal";
+import { useForm, FieldValues, SubmitHandler } from "react-hook-form";
+import { useState } from "react";
+import Input from "./Input";
+import Button from "./Button";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+  const [isLoading, setIsLoading] = useState(false);
+
+  const { register, handleSubmit, reset } = useForm<FieldValues>({
+    defaultValues: {
+      author: "",
+      title: "",
+      song: null,
+      image: null,
+    },
+  });
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      reset();
+      uploadModal.onClose();
+    }
+  };
+
+  const onSubmit: SubmitHandler<FieldValues> = async (values) => {};
+
+  return (
+    <Modal
+      title="Add a song"
+      description="Upload a mp3 file"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+        <Input
+          id="title"
+          disabled={isLoading}
+          {...register("title", { required: true })}
+          placeholder="Song title"
+        />
+        <Input
+          id="author"
+          disabled={isLoading}
+          {...register("author", { required: true })}
+          placeholder="Song author"
+        />
+
+        <div>
+          <div className="pb-1">Select a song file</div>
+          <Input
+            id="song"
+            type="file"
+            disabled={isLoading}
+            accept=".mp3"
+            {...register("song", { required: true })}
+          />
+        </div>
+
+        <div>
+          <div className="pb-1">Select a song file</div>
+          <Input
+            id="image"
+            type="file"
+            disabled={isLoading}
+            accept="image/*"
+            {...register("image", { required: true })}
+          />
+        </div>
+
+        <Button disabled={isLoading} type="submit">
+          Create
+        </Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default UploadModal;
